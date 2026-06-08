21b775f2378a3de4922dba6cf3fdc56ddf6cbc86
Julien FAURE <julien.faure@iaca-electronique.com>
2026-06-08 16:56:29 +0200

    Add rotation method to EditableImage for uncropped rotation on transparent canvas

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ impl App {
 
         let mut image = ImageEditableImage::from_nothing(1920, 1080);
         image.set_background_from_color((255, 255, 255));
-        image.add_scalable_object_from_file("toto.png", 0,0, 1.0,0.0);
+        image.add_scalable_object_from_file("toto.png", 0,0, 0.3,60.0);
+        // image.add_scalable_object_from_file("toto.png", 0,0, 0.3,0.0);
+        // image.add_scalable_object_from_file("toto.png", 0,0, 0.3,90.0);
         image.save("output.png");
         Ok(())
     }
