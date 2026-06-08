500ce73d6b89b155cc1fb69008656c6b415ba9cd
Julien FAURE <julien.faure@iaca-electronique.com>
2026-06-08 15:44:30 +0200

    Add image editing infrastructure with EditableImage trait and implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 pub mod settings;
+pub mod services;
+pub mod infrastructure;
 pub mod utils;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::ExitCode;
+use synthetic_data_generator_for_yolo::infrastructure::editable_image::{EditableImage, ImageEditableImage};
 use synthetic_data_generator_for_yolo::settings::VERSION;
 
 struct App {}
@@ -9,6 +10,11 @@ impl App {
     }
 
     async fn run(&self) -> Result<(), String> {
+
+        let mut image = ImageEditableImage::from_nothing(1920, 1080);
+        image.set_background_from_color((255, 255, 255));
+        image.add_scalable_object_from_file("toto.png", 0,0, 1.0,0.0);
+        image.save("output.png");
         Ok(())
     }
 }
